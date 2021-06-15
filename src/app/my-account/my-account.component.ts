2e5fd7581bd9ae9438118dd9aa2fb5752d3aa542
Jean <jeanlomibao18@gmail.com>
2021-06-14 23:30:23 -0600

    fixing navigation

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { CookieService } from '../services/cookie.service';
@@ -15,17 +16,22 @@ export class MyAccountComponent implements OnInit {
   public access: boolean;
   public id: number;
 
-  constructor(private _accountService: AccountService, private _authenticationService: AuthenticationService) { }
+  constructor(private _accountService: AccountService, private _authenticationService: AuthenticationService, 
+    private _router: Router) { }
 
   ngOnInit(): void {
     this.id = null;
-    this._authenticationService.populateAccountInfo().then((value) => {
-      this.email = this._authenticationService.currentUser.email;
-      this.name = this._authenticationService.currentUser.firstName + ' ' + this._authenticationService.currentUser.lastName;
-      this.access = this._authenticationService.currentUser.schoolAccessAllowed;
-      this.id = this._authenticationService.currentUser.id;
-      this.role = this._authenticationService.currentUser.role;
-    });
+    if (this._authenticationService.authenticated()) {
+      this._authenticationService.populateAccountInfo().then((value) => {
+        this.email = this._authenticationService.currentUser.email;
+        this.name = this._authenticationService.currentUser.firstName + ' ' + this._authenticationService.currentUser.lastName;
+        this.access = this._authenticationService.currentUser.schoolAccessAllowed;
+        this.id = this._authenticationService.currentUser.id;
+        this.role = this._authenticationService.currentUser.role;
+      });
+    } else {
+      this._router.navigateByUrl("/login");
+    }
   }
 
 }
